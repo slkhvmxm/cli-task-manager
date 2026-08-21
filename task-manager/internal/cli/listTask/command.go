4fa07ef21bcbced2f515b7a0a93ae76b56de89b7
Maxim Salikhov <fanny2man@gmail.com>
2026-08-21 21:09:11 +0300

    add variative listTask arg

--- task-manager/internal/cli/listTask/command.go
+++ task-manager/internal/cli/listTask/command.go
@@ -10,6 +10,7 @@ func NewCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "listTask [task name]",
 		Short: "A brief description of your command",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var t models.Task
 
@@ -19,7 +20,36 @@ func NewCommand() *cobra.Command {
 				return err
 			}
 
-			utils.PrintTable(tasks)
+			var sortedTasks []models.Task
+
+			if len(args) == 0 {
+				utils.PrintTable(tasks)
+				return nil
+			}
+
+			switch args[0] {
+			case "done":
+				for i := range tasks {
+					if tasks[i].Status == "done" {
+						sortedTasks = append(sortedTasks, tasks[i])
+						utils.PrintTable(sortedTasks)
+					}
+				}
+			case "in-progress":
+				for i := range tasks {
+					if tasks[i].Status == "in-progress" {
+						sortedTasks = append(sortedTasks, tasks[i])
+						utils.PrintTable(sortedTasks)
+					}
+				}
+			case "comleted":
+				for i := range tasks {
+					if tasks[i].Status == "completed" {
+						sortedTasks = append(sortedTasks, tasks[i])
+						utils.PrintTable(sortedTasks)
+					}
+				}
+			}
 			return nil
 		},
 	}
